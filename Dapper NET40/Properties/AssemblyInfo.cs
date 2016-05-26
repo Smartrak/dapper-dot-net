c10bb61b07749a9138e4c3ee8fdb81b4cf2a6f1b
Alena Choong <alena@smartrak.co.nz>
2016-05-26 12:52:56 +1200

    Pushing out new assembly version for Dapper and Dapper.Contrib

--- Dapper NET40/Properties/AssemblyInfo.cs
+++ Dapper NET40/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.43.0.0")]
-[assembly: AssemblyFileVersion("1.43.0.0")]
-[assembly: AssemblyInformationalVersion("1.43.0-smartrak1")]
+[assembly: AssemblyVersion("1.44.0.0")]
+[assembly: AssemblyFileVersion("1.44.0.0")]
+[assembly: AssemblyInformationalVersion("1.44.0-smartrak")]
